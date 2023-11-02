d73fc01f839180661292d2bd2940a0b22ecc2985
MikkelvtK <michielvtklooster@gmail.com>
2023-11-02 12:55:04 +0100

    chore: format code

--- lib/iss_api/location.ex
+++ lib/iss_api/location.ex
@@ -3,14 +3,21 @@ defmodule IssApi.Location do
 
   @type longitude :: float()
   @type latitude :: float()
-  @type t :: %__MODULE__{timestamp: non_neg_integer(), position: {longitude(), latitude()}}
+  @type t :: %__MODULE__{
+          timestamp: non_neg_integer(),
+          position: {longitude(), latitude()}
+        }
 
   @enforce_keys [:timestamp, :position]
   defstruct [:timestamp, :position]
 
-  @spec new(non_neg_integer(), {longitude(), latitude()}) :: t() | {:error, String.t()}
+  @spec new(non_neg_integer(), {longitude(), latitude()}) 
+          :: t() | {:error, String.t()}
   def new(timestamp, {longitude, latitude})
-      when is_integer(timestamp) and timestamp > 0 and is_float(longitude) and is_float(latitude) do
+      when is_integer(timestamp) 
+        and timestamp > 0 
+        and is_float(longitude) 
+        and is_float(latitude) do
     %__MODULE__{
       timestamp: timestamp,
       position: {longitude, latitude}
