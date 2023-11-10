950bf5e5dcad311f3588a462155434a76f3f06d8
MikkelvtK <mvtklooster@hotmail.com>
2023-11-10 16:31:21 +0100

    fix: refactor using HTTPoison

--- lib/iss_api/client.ex
+++ lib/iss_api/client.ex
@@ -1,10 +1,12 @@
 defmodule IssApi.Client do
   @moduledoc false
 
+  @env Mix.env()
+
   @spec fetch(IssApi.Collector.url()) ::
           {:ok, IssApi.Parser.json()} | {:error, IssApi.error()}
   def fetch(url) do
-    http_client().get(url)
+    http_client(@env).get(url)
     |> handle_response
   end
 
@@ -20,13 +22,9 @@ defmodule IssApi.Client do
     {:error, {:httpoison_error, reason}}
   end
 
-  defp http_client do
-    httpoison = Application.get_env(:iss_api, :http_client)
-
-    if httpoison == nil do
-      Application.put_env(:iss_api, :http_client, HTTPoison)
-    end
-
+  defp http_client(env) when env == :test do 
     Application.get_env(:iss_api, :http_client)
   end
+
+  defp http_client(_env), do: HTTPoison
 end
